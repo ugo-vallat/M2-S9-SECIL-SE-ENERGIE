34021512f3ca7ddabcc53cb6b87bfe7739c47977
Ugo Vallat <ugo.vallat.pro@gmail.com>
2025-11-11 22:39:38 +0100

    [GIT] add mojitos

--- init_home.sh
+++ init_home.sh
@@ -10,7 +10,7 @@ git config --global user.email "ugo.vallat@univ-tlse3.fr"
 git config --global core.editor "vim"
 git config --global alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit"
 git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
-git config --global alias.subup "submodule --update --init"
+git config --global alias.subup "submodule update --init --recursive"
 git pull
 git subup
 

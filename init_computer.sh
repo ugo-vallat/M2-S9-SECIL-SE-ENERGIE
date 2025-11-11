129a082afc865d77ab4392380b7f43ea34e1ae49
Ugo Vallat <ugo.vallat.pro@gmail.com>
2025-11-11 22:44:11 +0100

    [SC] add mojitos config

--- init_computer.sh
+++ init_computer.sh
@@ -1,8 +1,17 @@
 # Execute this script in his directory !
+sudo-g5k echo ""
 
-sudo cp mojitos/bin/mojitos /usr/bin/
+
+# config mojitos
+cd mojitos
+./configure.sh
+make
+sudo cp bin/mojitos /usr/bin/
 sudo sh -c 'echo 0 >/proc/sys/kernel/perf_event_paranoid'
 sudo chmod o+r /sys/class/powercap/intel-rapl/*/*
 sudo chmod o+r /sys/class/powercap/intel-rapl/*/*/*
+cd -
+
+
 
 

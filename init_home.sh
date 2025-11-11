9c5305e57cf1e18ff4ce494627f678d22fe82774
Ugo Vallat <ugo.vallat.pro@gmail.com>
2025-11-11 22:37:32 +0100

    [SC] update init_home.sh

--- init_home.sh
+++ init_home.sh
@@ -5,9 +5,15 @@ cp .bashrc ~/.bashrc
 source .bashrc
 
 # git
+git config --global user.name "Ugo VALLAT"
+git config --global user.email "ugo.vallat@univ-tlse3.fr"  
+git config --global core.editor "vim"
 git config --global alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit"
 git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
-git config --global core.editor "vim"
+git config --global alias.subup "submodule --update --init"
+git pull
+git subup
+
 
 
 

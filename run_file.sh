14996af76709903d1d26199a74993b74991bd5d3
Ugo Vallat <ugo.vallat.pro@gmail.com>
2025-11-11 23:16:40 +0100

    [SC] run script

---
+++ run_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Vérifier le nombre d'arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+freq=10
+input="$1"
+output="mojitos_$2"
+
+./mojitos -u -f "$freq" -o "tmp/local_output/$output" -- sleep 5
